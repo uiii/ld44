0762a0c9b622cf68221b933583c914e5a5011c83
Richard Jedlička <jedlicka.r@gmail.com>
2019-05-01 23:34:09 +0200

    cleaning

--- Assets/LargeIntestine.cs
+++ Assets/LargeIntestine.cs
@@ -14,14 +14,9 @@ public class LargeIntestine : MonoBehaviour
 	public float contractionPosition = float.MaxValue;
 	public float contractionRange = 2f;
 	public float contractionSpeed = 1f;
-	public float maxContraction = 0.2f;
-	public float stercusPosition = 0;
-	public float stercusLength = 3;
 	public float stercusMovementSpeed = 0.3f;
 
-	protected LargeIntestinePart[] partsObsolete;
-	public LargeIntestinePart2[] parts;
-	public float[] stercusPositions = new float[] {0, 15};
+	public LargeIntestinePart[] parts;
 
 	public Stercus[] stercuses
 	{
@@ -34,11 +29,7 @@ public class LargeIntestine : MonoBehaviour
 
 	void Start()
 	{
-		parts = GetComponentsInChildren<LargeIntestinePart2>();
-
-		for (int i = 0; i < parts.Length - 1; ++i) {
-			parts[i].nextPart = parts[i + 1];
-		}
+		parts = GetComponentsInChildren<LargeIntestinePart>();
 	}
 
 	void Update()
@@ -73,11 +64,6 @@ public class LargeIntestine : MonoBehaviour
 	{
 		if (isContracting) {
 			contractionPosition += Time.deltaTime * contractionSpeed;
-			/*if (contractionPosition >= 0 && contractionPosition < parts.Length) {
-				Debug.Log((int) contractionPosition);
-				parts[(int) contractionPosition].Contract();
-				parts[(int) contractionPosition].SqueezeOutStercus(Time.deltaTime * stercusMovementSpeed);
-			}*/
 
 			for (int i = 0; i < parts.Length; ++i) {
 				var part = parts[i];
@@ -90,54 +76,6 @@ public class LargeIntestine : MonoBehaviour
 			}
 
 			body.nutritionPercent += Time.deltaTime * 0.04f;
-
-			//foreach (var stercus in stercuses.Where(s => s.transform.position == parts[parts.Length - 1].transform.position).ToList()) {
-				//Destroy(stercus.gameObject);
-			//}
-		}
-
-		return;
-
-		for (int i = 0; i < partsObsolete.Length; ++i) {
-			var part = partsObsolete[i];
-
-			var scaleWieght = 1 - Mathf.Clamp01(Mathf.Abs(contractionPosition - i) / contractionRange);
-
-			//part.SetStercusPosition(1 - Mathf.Clamp01(i - (stercusPosition - stercusLength / 2f)));
-			//part.SetStercusPosition(Mathf.Clamp01(stercusPosition - i) + 0.5f);
-
-			/*float ss = Mathf.Clamp01(stercusPosition - i - stercusLength / 2f + 0.5f);
-			float se = Mathf.Clamp01(stercusPosition - i + stercusLength / 2f + 0.5f);
-			float sp = (se + ss) / 2f;
-
-			part.SetStercusPosition(sp);*/
-			part.Scale(new Vector3(1f - maxContraction * scaleWieght, 1f, 1f));
-		}
-
-		MoveStercus();
-	}
-
-	protected void MoveStercus()
-	{
-		foreach (var part in partsObsolete) {
-			part.SetStercusPosition(0);
-		}
-
-		for (int i = 0; i < stercusPositions.Length; ++i) {
-			if (isContracting && Mathf.Abs(contractionPosition - stercusPositions[i]) <= stercusLength / 2f) {
-				stercusPositions[i] += Time.deltaTime * stercusMovementSpeed;
-			}
-
-			int startPartIndex = Mathf.FloorToInt(Mathf.Max(0, stercusPositions[i] - stercusLength / 2f + 0.5f));
-			int endPartIndex = Mathf.CeilToInt(Mathf.Min(partsObsolete.Length - 1, stercusPositions[i] + stercusLength / 2f + 0.5f));
-
-			for (int j = startPartIndex; j < endPartIndex + 1; ++j) {
-				float ss = Mathf.Clamp01(stercusPositions[i] - j - stercusLength / 2f + 0.5f);
-				float se = Mathf.Clamp01(stercusPositions[i] - j + stercusLength / 2f + 0.5f);
-				float sp = (se + ss) / 2f;
-
-				partsObsolete[j].SetStercusPosition(sp);
-			}
 		}
 	}
 }
--- Assets/LargeIntestinePart.cs
+++ Assets/LargeIntestinePart.cs
@@ -4,46 +4,22 @@ using UnityEngine;
 
 public class LargeIntestinePart : MonoBehaviour
 {
-	public Transform stercusMask;
+	public float maxContraction = 0.2f;
 
-	private Vector3 originalScale;
-	private Vector3 originalMaskScale;
-
-	public float stercusPosition;
+	protected Vector3 originalScale;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		originalScale = transform.localScale;
-		originalMaskScale = stercusMask.localScale;
-
-		stercusMask.localScale = new Vector3(
-			stercusMask.localScale.x,
-			0.2f,
-			1f
-		);
 	}
 
-	// Update is called once per frame
-	void Update()
+	public void Contract(float percent = 1f)
 	{
-
-	}
-
-	public void Scale(Vector3 scale) {
 		transform.localScale = new Vector3(
-			originalScale.x * scale.x,
-			originalScale.y * scale.y,
+			originalScale.x * (1f - maxContraction * percent),
+			originalScale.y,
 			1f
 		);
-	}
-
-	public void SetStercusPosition(float percent)
-	{
-		stercusPosition = percent;
-		float maskPercent = -2f * Mathf.Abs(percent - 0.5f) + 1f;
-
-		stercusMask.localScale = new Vector3(originalMaskScale.x * maskPercent, originalMaskScale.y * maskPercent, 1f);
-		stercusMask.localPosition = new Vector3(0f, percent * 0.482f - 0.241f, 0f);
 	}
 }
